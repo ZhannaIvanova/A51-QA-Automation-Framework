08eaad93fa17dc65982ab32d1bec51721eb7a11c
ZhannaIvanova <148368740+ZhannaIvanova@users.noreply.github.com>
2023-10-21 13:40:52 -0400

    Homework16
    Registration

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class Homework16 {
+    @Test
+    public void registrationNavigation() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://qa.koel.app";
+        driver.get(url);
+        WebElement registrationNavigation = driver.findElement(By.cssSelector("button[type='registration/Forgot password']"));
+        System.out.println("Did this get run?");
+        registrationNavigation.click();
+        WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatar.isDisplayed());
+        driver.quit();
+    }
+}
