edfc3cf30a928cce93fb586500ffa2c70468967f
ZhannaIvanova <148368740+ZhannaIvanova@users.noreply.github.com>
2024-01-29 22:04:47 -0500

    HW26
    first

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,59 +1,122 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.time.Duration;
 
 
-
-
 public class BaseTest {
-    //added for cloud test
 
+    // DataProviders Start here
 
-    private static final ThreadLocal<WebDriver> THREAD_LOCAL = new ThreadLocal<>();
-    //added for cloud test
-    private WebDriver driver;
-
-    //added for cloud test
-    public static WebDriver getThreadLocal(){
-        return THREAD_LOCAL.get();
+    @DataProvider(name="LoginData")
+    public Object[][] getDataFromDataProvider(){
+        return new Object[][]{
+                {"zhanna.ivanova@testpro.io", "12345678"},
+                {"invalidemail@class.com", "12345678"},
+                {"zhanna.ivanova@testpro.io", "InvalidPassword"},
+                {"",""}
+        };
     }
 
-    /*public static WebDriver getDriver(){
-        return threadDriver.get();
-    }*/
+    @DataProvider(name="excel-data")
+    public Object[][] excelDP() throws IOException {
+        Object [][] arrObj;
+        arrObj = getExcelData("./src/test/resources/test.xlsx", "Sheet1");
+        return arrObj;
+    }
 
-    public static String url;
-    public WebDriverWait wait;
+
+    public static WebDriver driver = null;
+    public String url = "https://qa.koel.app";
+
+    public static WebDriverWait wait = null;
+
     public static Actions actions = null;
 
+    private static final ThreadLocal <WebDriver> threadDriver = new ThreadLocal<>();
+
+    //References End here
+
+
+    @BeforeSuite
+    static void setupClass() {
+//        WebDriverManager.chromedriver().setup();
+    }
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    //added for cloud test
-    public void setUpBrowser(@Optional String baseURL) throws MalformedURLException {
-        THREAD_LOCAL.set(pickBrowser(System.getProperty("browser")));
-        THREAD_LOCAL.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        THREAD_LOCAL.get().manage().window().maximize();
-        THREAD_LOCAL.get().manage().deleteAllCookies();
-        getThreadLocal().get(baseURL);
-        System.out.println("Browser setup by Thread " + Thread.currentThread().getId() + " and Driver reference is: " + getThreadLocal());
+    public void setupBrowser(String BaseURL) throws MalformedURLException {
+        threadDriver.set(pickBrowser(System.getProperty("browser")));
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        navigateToLoginPage(BaseURL);
     }
+
+    public static WebDriver getDriver(){
+        return threadDriver.get();
+    }
+
+    public static WebDriver pickBrowser(String browser) throws MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://10.0.0.206:4444";
+
+        switch (browser){
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new EdgeDriver(edgeOptions);
+
+            //Selenium Grid
+            case "grid-edge": // gradle cleam test -Dbrowser=grid-edge
+                caps.setCapability("browser", "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+
+            case "grid-firefox": // gradle clean test -Dbrowser=grid-firefox
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+            case "grid-chrome": // gradle clean test -Dbrowser=grid-chrome
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+            case "cloud":
+                return lambdaTest();
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                return driver = new ChromeDriver(options);
+        }
+    }
+
     //added for cloud test
     public WebDriver lambdaTest() throws MalformedURLException{
         String username = "zhanna.ivanova";
@@ -69,42 +132,70 @@ public class BaseTest {
         caps.setCapability("plugin", "git-testing");
         return new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
     }
-    public WebDriver pickBrowser(String browser) throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        String gridURL = "http://10.2.127.17:4444";
-
-        switch (browser) {
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                FirefoxOptions optionsFirefox = new FirefoxOptions();
-                optionsFirefox.addArguments("-private");
-                return driver = new FirefoxDriver(optionsFirefox);
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                return driver = new EdgeDriver();
-            case "grid-firefox":
-                capabilities.setCapability("browserName", "firefox");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
-            case "grid-edge":
-                capabilities.setCapability("browserName", "edge  ");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
-            case "grid-chrome":
-                capabilities.setCapability("browserName", "chrome");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
-            case "cloud":
-                return lambdaTest();
-            default:
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions optionsChrome = new ChromeOptions();
-                optionsChrome.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
-                optionsChrome.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-                return driver = new ChromeDriver(optionsChrome);
-        }
-    }
-
     @AfterMethod
-    public void tearDown() {
-        THREAD_LOCAL.get().close();
-        THREAD_LOCAL.remove();
+    public void tearDown(){
+        threadDriver.get().close();
+        threadDriver.remove();
     }
+
+
+    public void navigateToLoginPage(){
+        driver.get(url);
+    }
+
+    public void navigateToLoginPage(String BaseURL){
+        getDriver().get(BaseURL);
+    }
+
+    public void provideEmail(String email){
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
+        //WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void providePassword(String password){
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickSubmit(){
+        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
+        submit.click();
+    }
+
+
+    public String [][] getExcelData(String fileName, String sheetName) {
+        String [][] data = null;
+        try{
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            XSSFWorkbook wb = new XSSFWorkbook(fileInputStream);
+            XSSFSheet sheet = wb.getSheet(sheetName);
+            XSSFRow row = sheet.getRow(0);
+
+            int numOfRows = sheet.getPhysicalNumberOfRows();
+            int numOfColumns = row.getLastCellNum();
+
+            XSSFCell cell;
+
+            data = new String[numOfRows-1][numOfColumns];
+
+            for(int i = 1; i < numOfRows; i++){
+                for (int j = 0; j < numOfColumns; j++){
+                    row = sheet.getRow(i);
+                    cell = row.getCell(j);
+                    data [i-1][j] = cell.getStringCellValue();
+                }
+            }
+        } catch(Exception e){
+            System.out.println("Something went wrong." +e);
+        }
+        return data;
+    }
+
+
+
+
+
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,53 +1,114 @@
 import Pages.HomePage;
 import Pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 public class LoginTests extends BaseTest {
 
-    @Test
-    public static void loginEmptyEmailPasswordTest() {
-        //changed for cloud test
-        LoginPage loginPage = new LoginPage(getThreadLocal());
-        loginPage.provideEmail("");
-        loginPage.providePassword("12345678");
-        loginPage.clickSubmitBtn();
-        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
-    }
-    @Test
-    public  static void loginWrongPasswordTest() {
-        LoginPage loginPage = new LoginPage(getThreadLocal());
-        loginPage.provideEmail("zhanna.ivanova@testpro.io");
-        loginPage.providePassword("1234678");
-        loginPage.clickSubmitBtn();
-        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
-
-    }
-    @Test
-    public static void loginEmptyPasswordTest(){
-        LoginPage loginPage = new LoginPage(getThreadLocal());
-        loginPage.provideEmail("zhanna.ivanova@testpro.io");
-        loginPage.providePassword("");
-        loginPage.clickSubmitBtn();
-        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
-
-    }
+    //
 
     @Test
-    public static void loginWrongEmailTest(){
-        LoginPage loginPage = new LoginPage(getThreadLocal());
-        loginPage.provideEmail("zhana.ivanova@testpro.io");
-        loginPage.providePassword("12345678");
-        loginPage.clickSubmitBtn();
-        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
-    }
-    @Test
-    public void loginSucceedTest(){
-        LoginPage loginPage = new LoginPage(getThreadLocal());
-        HomePage homePage = new HomePage(getThreadLocal());
-        loginPage.provideLoginSucceed();
+    public void loginSuccessTest() {
+
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+
+        loginPage.provideEmail("demo@class.com")
+                .providePassword("te$t$tudent")
+                .clickSubmitBtn();
+//        WebElement avatar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[class='avatar']")));
+//        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
         Assert.assertTrue(homePage.getUserAvatar());
     }
-}
+    @Test
+    public void loginInvalidCredentials(){
+        LoginPage loginPage = new LoginPage(getDriver());
 
+        loginPage.provideEmail("")
+                .providePassword("te$t$tudent")
+                .clickSubmitBtn();
+        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
+    }
+
+    /*@Test(dataProvider = "LoginData")
+    public void loginTests(String email, String password) throws InterruptedException {
+        navigateToLoginPage();
+        provideEmail(email);
+        providePassword(password);
+        Thread.sleep(2000);
+        clickSubmit();
+        Thread.sleep(2000);
+        //https://qa.koel.app/#!/home
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+    }*/
+
+    /*@Test
+    public void loginValidEmailPassword(){
+        //Steps
+        navigateToLoginPage();
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        WebElement avatar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[class='avatar']")));
+        //WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+        //Expected Result
+        Assert.assertTrue(avatar.isDisplayed());
+    }*/
+
+  /*  @Test
+    public void loginInvalidEmailValidPassword(){
+//        navigateToLoginPage();
+//        provideEmail("invalidemail@class.com");
+//        providePassword("te$t$tudent");
+//        clickSubmit();
+//        WebElement avatar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[class='avatar']")));
+//        //Expected Result
+//        Assert.assertTrue(avatar.isDisplayed());
+
+        LoginPage loginPage = new LoginPage(getDriver());
+    }*/
+
+    //@Test
+  /*  public void loginValidEmailNoPassword() throws InterruptedException{
+        //Pr-Conditions
+
+        navigateToLoginPage();
+        provideEmail("invalidemail@class.com");
+        clickSubmit();
+
+        WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+        //Expected Result
+        Assert.assertTrue(avatar.isDisplayed());
+    }*/
+
+    /*@Test(dataProvider = "excel-data")
+    public void loginWithExcelData(String email, String password){
+        try{
+        //Step 1 Enter EMail
+        Thread.sleep(2000);
+        provideEmail(email);
+        //Step 2: Enter Password
+        providePassword(password);
+        Thread.sleep(2000);
+
+        //Step 3: Click on Submit.
+        clickSubmit();
+        Thread.sleep(2000);
+
+        WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+        //Expected Result
+        Assert.assertTrue(avatar.isDisplayed());
+
+        } catch(Exception e){
+            Reporter.log("Unable to login with Excel Data for an unknown reason." +e);
+        }
+    }*/
+
+    public void waitForAnElementToBeVisible(String cssLocator){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("")));
+    }
+
+
+}
